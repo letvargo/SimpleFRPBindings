31174bf25f55712de0f10b39cecaea30b22f81d2
letvargo <letvargo@gmail.com>
2015-04-29 22:59:55 -0700

    Initial Commit

---
+++ SimpleFRPBindings/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SimpleFRPBindings
+//
+//  Created by developer on 4/29/15.
+//  Copyright (c) 2015 letvargo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
